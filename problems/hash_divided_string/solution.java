22b37a30e48c9214083b7adc5b4638bf316127f4
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2024-08-31 15:14:07 +0000

    Sync LeetCode submission - Hash Divided String (java)

---
+++ problems/hash_divided_string/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String stringHash(String s, int k) {
+        int n = s.length();
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < n; i += k) {
+            String substring = s.substring(i, i + k);
+            int sum = 0;
+            
+            for (char c : substring.toCharArray()) {
+                sum += c - 'a';
+            }
+            
+            int hashedChar = sum % 26;
+            result.append((char) (hashedChar + 'a')); 
+        }
+        return result.toString();
+    }
+}
