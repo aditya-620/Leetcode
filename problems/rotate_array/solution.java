5f2c7f162d3dbb0384082f09389e38e3d0bc41b3
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2025-04-01 18:58:04 +0000

    Sync LeetCode submission - Rotate Array (java)

--- problems/rotate_array/solution.java
+++ problems/rotate_array/solution.java
@@ -1,12 +1,32 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        // int n = nums.length;
+        // int arr[] = new int[n];
+        // for(int i=0; i<n; i++){
+        //     arr[(i+k)%n] = nums[i];
+        // }
+        // for(int i=0; i<n; i++){
+        //     nums[i] = arr[i];
+        // }
+
+        //optimal sol
+        k = k % nums.length;
         int n = nums.length;
-        int arr[] = new int[n];
-        for(int i=0; i<n; i++){
-            arr[(i+k)%n] = nums[i];
-        }
-        for(int i=0; i<n; i++){
-            nums[i] = arr[i];
+        // Reverse first n-k elements
+        Reverse(nums, 0, n - k - 1);
+        // Reverse last k elements
+        Reverse(nums, n - k, n - 1);
+        // Reverse whole array
+        Reverse(nums, 0, n - 1);
+    }
+    public static void Reverse(int[] arr, int start, int end) {
+        
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
     }
 }
