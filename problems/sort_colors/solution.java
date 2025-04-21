e7ad9b8e1b5968edad7959b0e6591c2f5af1d89b
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2025-04-21 17:42:37 +0000

    Sync LeetCode submission - Sort Colors (java)

--- problems/sort_colors/solution.java
+++ problems/sort_colors/solution.java
@@ -1,21 +1,42 @@
 class Solution {
     public void sortColors(int[] nums) {
-        HashMap<Integer, Integer> count = new HashMap<>();
-        count.put(0, 0);
-        count.put(1, 0);
-        count.put(2, 0);
+        // HashMap<Integer, Integer> count = new HashMap<>();
+        // count.put(0, 0);
+        // count.put(1, 0);
+        // count.put(2, 0);
 
-        for (int num : nums) {
-            count.put(num, count.get(num) + 1);
-        }
+        // for (int num : nums) {
+        //     count.put(num, count.get(num) + 1);
+        // }
 
-        int idx = 0;
-        for (int i = 0; i < 3; i++) {
-            int freq = count.get(i);
-            for (int j = 0; j < freq; j++) {
-                nums[idx] = i;
-                idx++;
+        // int idx = 0;
+        // for (int i = 0; i < 3; i++) {
+        //     int freq = count.get(i);
+        //     for (int j = 0; j < freq; j++) {
+        //         nums[idx] = i;
+        //         idx++;
+        //     }
+        // }
+
+        //Using Dutch National Flag Algo
+        int n = nums.length;
+        int low = 0, mid = 0, high = n-1;   
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1){
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
             }
-        }        
+        }     
     }
 }
