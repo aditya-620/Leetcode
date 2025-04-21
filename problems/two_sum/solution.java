4615549a2c1d8c03ccc3523066c4f017b5fdd7e6
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2025-04-21 16:47:52 +0000

    Sync LeetCode submission - Two Sum (java)

--- problems/two_sum/solution.java
+++ problems/two_sum/solution.java
@@ -12,5 +12,30 @@ public class Solution {
             mpp.put(nums, i);
         }
         return arr;
+
+
+        //optimal approach 
+        //but here can't be used as we have to return the original index of the array
+
+        // Arrays.sort(numbers);
+        // int n = numbers.length;
+        // int left = 0;
+        // int right = n-1;
+        // int arr[] = new int[2];
+        // while(left < right){
+        //     int sum = numbers[left] + numbers[right];
+        //     if(sum == target){
+        //         arr[0] = left;
+        //         arr[1] = right;
+        //         return arr;
+        //     } else if (sum < target){
+        //         left++;
+        //     } else {
+        //         right--;
+        //     }
+        // }
+        // arr[0] = -1;
+        // arr[1] = -1;
+        // return arr;
     }
 }
