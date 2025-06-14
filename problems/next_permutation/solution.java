661c11e6ccd14092b90299902d394a944176cc29
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2025-06-14 16:42:52 +0000

    Sync LeetCode submission - Next Permutation (java)

--- problems/next_permutation/solution.java
+++ problems/next_permutation/solution.java
@@ -2,35 +2,32 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int idx = -1;
         int n = nums.length;
-
-        for(int i = n-2; i >= 0; i--){
+        for(int i=n-2; i>=0; i--){
             if(nums[i] < nums[i+1]){
                 idx = i;
                 break;
             }
         }
         if(idx == -1){
-            reverse(nums, 0, n-1);
+            reverse(nums,0,n-1);
             return;
         }
 
-        for(int i= n-1; i>idx; i--){
+        for(int i=n-1; i>idx; i--){
             if(nums[idx] < nums[i]){
-                int temp = nums[i];
-                nums[i] = nums[idx];
-                nums[idx] = temp;
+                int temp = nums[idx];
+                nums[idx] = nums[i];
+                nums[i] = temp;
                 break;
             }
         }
-
         reverse(nums, idx+1, n-1);
     }
-
-    public static void reverse(int arr[], int idx, int n){
+    public static void reverse(int nums[], int idx, int n){
         while(idx < n){
-            int temp = arr[idx];
-            arr[idx] = arr[n];
-            arr[n] = temp;
+            int temp = nums[idx];
+            nums[idx] = nums[n];
+            nums[n] = temp;
             idx++;
             n--;
         }
