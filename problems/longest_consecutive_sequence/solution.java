c14d84108a0589334d141393e2c210684819c7bd
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2025-06-15 06:32:46 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (java)

---
+++ problems/longest_consecutive_sequence/solution.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        //better approach
+        int lastsmaller = Integer.MIN_VALUE;
+        int count = 0;
+        int longest = 0;
+        
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]-1 == lastsmaller){
+                count++;
+                lastsmaller = nums[i];
+            } else if (nums[i] != lastsmaller){
+                count = 1;
+                lastsmaller = nums[i];
+            }
+            longest = Math.max(longest,count);
+        }
+        return longest;
+
+        //brute force
+    //     int n = nums.length;
+    //     int count = 1;
+    //     int maxcount = 0;
+    //     if(n==0) return maxcount;
+
+    //     for(int i=0; i<n; i++){
+    //         int x = nums[i];
+    //         while(linearSearch(nums, x+1)){
+    //             x = x+1;
+    //             count++;
+    //         }
+    //         maxcount = Math.max(maxcount, count);
+    //         count = 1;
+    //     }
+    //     return maxcount;
+    }
+    // public static boolean linearSearch(int arr[], int x){
+    //     int n = arr.length;
+    //     for(int i=0; i<n; i++){
+    //         if(x == arr[i]){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+}
