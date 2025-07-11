0cf34d316310b2c6e6687933a3824e240b3d1eea
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2025-07-11 11:50:48 +0000

    Sync LeetCode submission - Rotate Image (java)

---
+++ problems/rotate_image/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        //transpose the matrix - using swaping
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //reversing the matrix row
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+        //Loops only till the middle of the row.
+        //This is because you are swapping elements from the ends inward.
+    }
+}
