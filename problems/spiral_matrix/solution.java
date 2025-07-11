ea3bbe71a01dc6fe4abe968a02c9606fcef73f17
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2025-07-11 12:59:37 +0000

    Sync LeetCode submission - Spiral Matrix (java)

--- problems/spiral_matrix/solution.java
+++ problems/spiral_matrix/solution.java
@@ -1,27 +1,47 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int x = 0;
-        int y = 0;
-        int dx = 1;
-        int dy = 0;
-        List<Integer> res = new ArrayList<>();
+        int sr = 0;
+        int sc = 0;
+        int er = matrix.length-1;
+        int ec = matrix[0].length-1;
+        ArrayList<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < rows * cols; i++) {
-            res.add(matrix[y][x]);
-            matrix[y][x] = -101; // the range of numbers in matrix is from -100 to 100
-
-            if (!(0 <= x + dx && x + dx < cols && 0 <= y + dy && y + dy < rows) || matrix[y+dy][x+dx] == -101) {
-                int temp = dx;
-                dx = -dy;
-                dy = temp;
+        //here && is taken so that m*n type matrix should also be countered
+        while(sr<=er && sc <= ec){
+            //top boundary
+            for(int i=sc; i<=ec; i++){
+                list.add(matrix[sr][i]);
             }
 
-            x += dx;
-            y += dy;
-        }
+            //right boundary
+            for(int i=sr+1; i<=er; i++){
+                list.add(matrix[i][ec]);
+            }
 
-        return res;        
+            //bottom boundary
+            for(int i=ec-1; i>=sc; i--){
+                //this cond occur when we have odd no. of spirals or odd n*n matrix
+                //we can't print the same value again, as it is already printed in top boundary
+                if(sr == er){  
+                    break;
+                }
+                list.add(matrix[er][i]);
+            }
+
+            //left boundary
+            for(int i=er-1; i>=sr+1; i--){
+                //already printed in right boundary
+                if(sc == ec){
+                    break;
+                }
+                list.add(matrix[i][sc]);
+            }
+
+            sr++;
+            sc++;
+            er--;
+            ec--;
+        }
+        return list;
     }
 }
