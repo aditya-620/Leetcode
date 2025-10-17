b07d5b609f72cbfdfd3ddf4873da591f7bcff4dc
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2025-10-17 12:18:26 +0000

    Sync LeetCode submission - Sum of Elements With Frequency Divisible by K (java)

---
+++ problems/sum_of_elements_with_frequency_divisible_by_k/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int sumDivisibleByK(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue() % k == 0){
+                sum = sum + (entry.getKey() * entry.getValue());
+            }
+        }
+
+        return sum;
+    }
+}
