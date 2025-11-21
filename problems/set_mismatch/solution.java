a741e6a1f28b7aa97d47afd0165bd2c631131e93
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2025-11-21 21:11:05 +0000

    Sync LeetCode submission - Set Mismatch (java)

---
+++ problems/set_mismatch/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            } else {
+                i++;
+            }
+        }
+        for(int j = 0; j < nums.length;j++){
+            if (nums[j] != j + 1) {
+                return new int[] {nums[j] , j+1} ;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    void swap(int[] arr , int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
