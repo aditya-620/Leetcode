8001d71d8ebe51b67257ed49815a3840ae0fb1c6
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2026-01-07 01:42:07 +0000

    Sync LeetCode submission - Trapping Rain Water (java)

--- problems/trapping_rain_water/solution.java
+++ problems/trapping_rain_water/solution.java
@@ -1,27 +1,25 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        //calculate the left max boundary 
-        int leftmax[] = new int[n];
-        leftmax[0] = height[0];
-        for(int i=1; i<n; i++){
-            leftmax[i] =Math.max(height[i],leftmax[i-1]);
+        int left[] = new int[n];
+        int right[] = new int[n];
+        int trapped = 0;
+
+        left[0] = height[0];
+        right[n-1] = height[n-1];
+
+        //left aux array
+        for(int i=1;i<n;i++){
+            left[i] = Math.max(height[i], left[i-1]);
         }
-        //calculate right max boundary
-        int rightmax[] = new int[n];
-        rightmax[n-1] = height[n-1];
+        //right aux array
         for(int i=n-2; i>=0; i--){
-            rightmax[i] = Math.max(height[i],rightmax[i+1]);
+            right[i] = Math.max(height[i], right[i+1]);
         }
-        int trappedwater = 0;
-        for(int i=0; i<n; i++){
-            int waterlevel = Math.min(rightmax[i],leftmax[i]);
-            trappedwater += waterlevel - height[i];
+        //cal trapped water
+        for(int i=0; i<n;i++){
+            trapped += (Math.min(left[i], right[i]) - height[i]);
         }
-        return trappedwater;
-    }
-    public void main(String args[]){
-        int height[] = {0,1,0,2,1,0,1,3,2,1,2,1};
-        System.out.println(trap(height));
+        return trapped;
     }
 }
