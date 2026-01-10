dda335416e1e0c20b1fb06e98528cd85c4cdaa2f
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2026-01-10 15:41:52 +0000

    Sync LeetCode submission - Missing Number (java)

--- problems/missing_number/solution.java
+++ problems/missing_number/solution.java
@@ -1,12 +1,23 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            if(nums[i] != i){
-                return i;
-            }
+        int sum = 0;
+        
+        for(int i=0; i<n; i++){
+            sum += nums[i];
         }
-        return n;
+        int arrsum = (n*(n+1))/2;
+
+        return arrsum - sum;
+
+        //O(nlogn)
+        // int n = nums.length;
+        // Arrays.sort(nums);
+        // for(int i=0;i<n;i++){
+        //     if(nums[i] != i){
+        //         return i;
+        //     }
+        // }
+        // return n;
     }
 }
