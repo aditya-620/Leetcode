2e405ea745ffffe10ad3e218933c9f113d6debf0
Aditya Rastogi <112893056+aditya-620@users.noreply.github.com>
2026-06-01 17:26:40 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array (java)

--- problems/remove_duplicates_from_sorted_array/solution.java
+++ problems/remove_duplicates_from_sorted_array/solution.java
@@ -1,17 +1,34 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // Length of the updated array
-        int count = 0;
-        // Loop for all the elements in the array
-        for (int i = 0; i < nums.length; i++) {
-            // If the current element is equal to the next element, we skip
-            if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
-                continue;
+        //two pointer
+        int i = 0;
+        int j = 1;
+        int c = 1;
+        while(j<nums.length){
+            if(nums[i] == nums[j]){
+                j++;
+            } else {
+                i++;
+                nums[i] = nums[j];
+                j++;
+                c++;
             }
-            // We will update the array in place
-            nums[count] = nums[i];
-            count++;
         }
-        return count;
+        return c;
+
+
+        
+        // int count = 0;
+        
+        // for (int i = 0; i < nums.length; i++) {
+        //     // If the current element is equal to the next element, we skip
+        //     if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+        //         continue;
+        //     }
+        //     // We will update the array in place
+        //     nums[count] = nums[i];
+        //     count++;
+        // }
+        // return count;
     }
 }
